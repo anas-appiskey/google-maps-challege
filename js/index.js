090888e9e054d70b200026afe2c4bf020f3d593a
anasali2193 <anasali2193@gmail.com>
2020-06-01 23:14:06 +0500

    add initMap function

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var map;
+function initMap() {
+  var losAngeles= {
+    lat :34.063380,
+    lng : -118.358080
+  } 
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: losAngeles,
+    zoom: 8
+  });
+}
+
+
+function displayStores(){
+    
+}
